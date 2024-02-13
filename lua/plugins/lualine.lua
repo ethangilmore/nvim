fbc9457db409eda865ea65d2ab78c96ac99a1773
ethangilmore <ethanmckgilmore@gmail.com>
2024-02-13 14:46:37 -0700

    Added lualine plugin

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,9 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+        require('lualine').setup({
+            options = { theme = 'codedark' },
+        })
+    end,
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+}
