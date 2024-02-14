d39d430b71469fbcf2e6b68c6b51674ce52915ac
ethangilmore <ethanmckgilmore@gmail.com>
2024-02-14 00:12:05 -0700

    Added indent-blankline plugin

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,7 @@ return {
     config = function()
         vim.keymap.set('n', '<Leader>g', ':G ')
         vim.keymap.set('n', '<Leader>ga', ':G add ')
+        vim.keymap.set('n', '<Leader>gr', ':G restore ')
         vim.keymap.set('n', '<Leader>gc', ':G commit -m ')
         vim.keymap.set('n', '<Leader>gs', function() vim.cmd('G status') end)
     end,
