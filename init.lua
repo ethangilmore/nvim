55f260feadfaa7a7175cb2b992619269a26c066b
ethangilmore <ethanmckgilmore@gmail.com>
2024-02-14 20:41:59 -0700

    Modified lsp diagnostics

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.signcolumn = "yes:1"
 
 -- lazy.nvim things
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
