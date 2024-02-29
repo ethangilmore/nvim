2d5a2778e11489f92b3c812865cbbcdf07ee4228
ethangilmore <ethanmckgilmore@gmail.com>
2024-02-28 23:43:22 -0700

    Added keybinds for down and up

--- lua/plugins/smoothie.lua
+++ lua/plugins/smoothie.lua
@@ -1,3 +1,7 @@
 return {
     'psliwka/vim-smoothie',
+    init = function()
+        vim.keymap.set({ 'n', 'i' }, '<C-j>', '<Plug>(SmoothieDownwards)')
+        vim.keymap.set({ 'n', 'i' }, '<C-k>', '<Plug>(SmoothieUpwards)')
+    end,
 }
